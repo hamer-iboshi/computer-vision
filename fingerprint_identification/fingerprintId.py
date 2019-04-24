2cce20c5a424d1a9dcb4577d65dfd3a67f29dcdf
Hamer Iboshi <hi15@inf.ufpr.br>
2019-04-24 14:05:39 -0300

    Try to make image enhancement

--- fingerprint_identification/fingerprintId.py
+++ fingerprint_identification/fingerprintId.py
@@ -29,12 +29,41 @@ class FingerprintId:
 			print(file_path)
 			img = Image.frombytes('L',self.size, open(file_path).read(), decoder_name='raw')
 			img.load()
-			img = np.asarray(img)
-			cv2.imshow(file,img)
+			cimg = np.asarray(img)
+			#cv2.imshow(file,img)
+			#cv2.waitKey(0)
+			eimg = self.enhacement(cimg)
+			cv2.imshow(file,eimg)
 			cv2.waitKey(0)
-			self.images.append(img)
+			self.images.append(eimg)
 			print(file[0:4])
 			self.images_classes.append(file[0:4])
+
+	def enhacement(self, img):
+		dimg = np.copy(img)
+		size_i = self.size[0]
+		size_j = self.size[1]
+		mean = img.mean()
+		variance = img.var()
+		s = 96
+		y = 95
+		alfa = 150 
+		while s > y:
+			mean = dimg.mean()
+			variance = dimg.var()
+			s = np.sqrt(variance)
+			print("S < Y ",s)
+			for i in range(0,size_i):
+				for j in range(0,size_j):
+					dimg[j][i]=alfa+y*((img[i][j]-mean)/s)
+			
+		print(dimg)
+		dimg = scale_image(dimg)
+		return dimg
+
+def scale_image(arr):
+	new_arr = ((arr - arr.min()) * (1/(arr.max() - arr.min())) * 255).astype('uint8')
+	return new_arr
 
 def main(argv):
 	fp = FingerprintId(DBPath['lindex'])
